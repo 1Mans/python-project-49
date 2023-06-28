1ad7a6a12fc0302f6e64890b1f6f5609491df887
1Mans <pigmalioon1@gmail.com>
2023-06-28 16:48:26 +0200

    fix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -25,12 +25,4 @@ def get_question_and_answer():
     question, answer = build_question(progression, progression_length)
     return question, answer
 
-# def get_question_and_answer():
-#     progression_length = random.randint(5, 10)
-#     progression = list(range(0, random.randint(30, 66), random.randint(2, 5)))
-#     if len(progression) > progression_length:
-#         progression = progression[:progression_length]
-#     answer = str(random.choice(progression))
-#     add_sep_to_progression = ' '.join(map(str, progression))
-#     question = add_sep_to_progression.replace(answer, '..', 1)
-#     return question, answer
+
