371c0aeadc0983af10be79501bedf2b9b6b42bb2
1Mans <pigmalioon1@gmail.com>
2023-07-07 09:07:52 +0200

    add files to gitignore

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,12 +5,12 @@ START_MIN = 0
 START_MAX = 30
 STEP_MIN = 2
 STEP_MAX = 5
+LENGTH_MIN = 5
+LENGTH_MAX = 10
 
 
-def generate_progression(length):
-    start = random.randint(START_MIN, START_MAX)
-    step = random.randint(STEP_MIN, STEP_MAX)
-    progression = list(range(start, start + length * step, step))
+def generate_progression(length, first_term, difference):
+    progression = [first_term + i * difference for i in range(length)]
     return progression
 
 
@@ -22,8 +22,9 @@ def build_question(progression, hidden_index):
 
 
 def get_question_and_answer():
-    progression_length = random.randint(5, 10)
-    progression = generate_progression(progression_length)
-    hidden_index = random.randint(0, len(progression) - 1)
-    question, answer = build_question(progression, hidden_index)
-    return question, answer
+    progression_length = random.randint(LENGTH_MIN, LENGTH_MAX)
+    first_term = random.randint(START_MIN, START_MAX)
+    difference = random.randint(STEP_MIN, STEP_MAX)
+    progression = generate_progression(progression_length, first_term, difference)
+    hidden_index = random.randint(0, progression_length - 1)
+    return build_question(progression, hidden_index)
